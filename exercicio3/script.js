c4d85d635c33363eed8e108b5a9300d65f819563
Julio238 <julio.cesarnascimentodos.santos@escola.pr.gov.br>
2025-05-21 09:19:47 -0300

    Add files via upload

---
+++ exercicio3/script.js
@@ -0,0 +1,75 @@
+//calculadoa simples com as 5 operações baica:
+//adição, subitração, multiplicação, divição.
+let num1;
+let num2;
+let resultado
+function sobra() {
+    num1 = Number(window.prompt('Digite um numero:'));
+    num2 = Number(window.prompt('Digite outro numero:'));
+
+    resultado = num1 % num2;
+    numero = resultado % 2;
+
+    alert("O resultado é " + resultado + ".");
+    if (numero == 0) {
+        alert ("Par");
+    }else {
+        alert ("impar")
+    }    
+}
+function multiplicacao() {
+    num1 = Number(window.prompt('Digite um numero:'));
+    num2 = Number(window.prompt('Digite outro numero:'));
+
+    resultado = num1 * num2;
+     numero = resultado % 2;
+    
+    alert("O resultado é " + resultado + ".");
+    if (numero == 0) {
+        alert ("Par");
+    }else {
+        alert ("impar")
+    }    
+}
+function adicao() {
+    num1 = Number(window.prompt('Digite um numero:'));
+    num2 = Number(window.prompt('Digite outro numero:'));
+
+    resultado = num1 + num2;
+     numero = resultado % 2;
+    
+    alert("O resultado é " + resultado + ".");
+    if (numero == 0) {
+        alert ("Par");
+    }else {
+        alert ("impar")
+    }    
+}
+function subitracao() {
+    num1 = Number(window.prompt('Digite um numero:'));
+    num2 = Number(window.prompt('Digite outro numero:'));
+
+    resultado = num1 - num2;
+     numero = resultado % 2;
+    
+    alert("O resultado é " + resultado + ".");
+    if (numero == 0) {
+        alert ("Par");
+    }else {
+        alert ("impar")
+    }    
+}
+function divisao() {
+    num1 = Number(window.prompt('Digite um numero:'));
+    num2 = Number(window.prompt('Digite outro numero:'));
+
+    resultado = num1 / num2;
+     numero = resultado % 2;
+    
+    alert("O resultado é " + resultado + ".");
+    if (numero == 0) {
+        alert ("Par");
+    }else {
+        alert ("impar")
+    }    
+}
